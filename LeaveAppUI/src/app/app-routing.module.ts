f14b83ba69ed0876ae7b70fe8001c0c86046a79f
Vimala <vimalapolagani00@gmail.com>
2025-07-03 12:53:01 +0530

    Created components (Homepage, NotifyLeave Page)

--- LeaveAppUI/src/app/app-routing.module.ts
+++ LeaveAppUI/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NotifyleaveComponent } from './notifyleave/notifyleave.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"homepage",component:HomepageComponent},
+  {path:"notifyleave",component:NotifyleaveComponent},
+  { path: '', redirectTo: '/homepage', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
