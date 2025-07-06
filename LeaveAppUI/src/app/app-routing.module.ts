07cc8b618ac221973dc14e39b0410275cdda4c4f
Bevin <bevin215@gmail.com>
2025-07-06 18:08:32 +0530

    Created success component

--- LeaveAppUI/src/app/app-routing.module.ts
+++ LeaveAppUI/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { NotifyleaveComponent } from './notifyleave/notifyleave.component';
+import { FormSubmittedComponent } from './form-submitted/form-submitted.component';
 
 const routes: Routes = [
   {path:"homepage",component:HomepageComponent},
   {path:"notifyleave",component:NotifyleaveComponent},
+  {path:"success",component:FormSubmittedComponent},
   { path: '', redirectTo: '/homepage', pathMatch: 'full' }
 ];
 
