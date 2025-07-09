808f7f36034f714080b6d8cb27f796e03694952a
Bevin215 <113708739+Bevin215@users.noreply.github.com>
2025-07-09 15:09:19 +0530

    Delete LeaveAppBackend/SpringBackend1 directory

--- LeaveAppBackend/SpringBackend1/src/main/java/com/example/demo/repository/LocationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Location;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface LocationRepository extends MongoRepository<Location, String> {
-    
-}
--- LeaveAppBackend/SpringBackend1/src/main/java/com/example/demo/repository/SUBLOBTEAMRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.SUBLOBTEAM;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface SUBLOBTEAMRepository extends MongoRepository<SUBLOBTEAM, String> {
-}
-
