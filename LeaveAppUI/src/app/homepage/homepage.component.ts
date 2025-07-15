2d898a599b3efeb2f7bf82ce72e7c116bb4b48bc
AbhiSathwik <abhisathwikbathala1289@gmail.com>
2025-07-15 18:51:09 +0530

    cache

--- LeaveAppUI/src/app/homepage/homepage.component.ts
+++ LeaveAppUI/src/app/homepage/homepage.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HolidayService } from '../Services/holiday.service';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
+  holidayDates: string[] = [];
 
+  constructor(private holidayService: HolidayService) {}
+
+  ngOnInit(): void {
+    this.loadHolidayDates();
+  }
+
+  loadHolidayDates(): void {
+    this.holidayService.getHolidayDates().subscribe(dates => {
+      this.holidayDates = dates;
+    });
+  }
+
+  refreshHolidayDates(): void {
+    this.holidayService.clearHolidayCache();
+    this.loadHolidayDates(); // Force re-fetch
+  }
 }
