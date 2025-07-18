1f33810ebe64284460598e58a7bf08923a3875bc
Ashifa-s <134677643+Ashifa-s@users.noreply.github.com>
2025-07-18 12:13:28 +0530

    Delete LeaveAppBackend/SpringBackend1 directory

--- LeaveAppBackend/SpringBackend1/src/main/java/com/example/demo/repository/HolidayRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Holiday;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface HolidayRepository extends MongoRepository<Holiday, String> {
-}
